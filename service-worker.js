bdc0cc9ba7baa341dded791a7b684eacc4c6d531
toniadelfos <163552172+toniadelfos@users.noreply.github.com>
2025-09-20 15:16:37 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,7 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(caches.open('jucap-hits-cache').then(function(cache) {
-    return cache.addAll([
-      '/',
-      '/index.html',
-      '/logo.jpg',
-      '/favicon.ico',
-      '/icon-192.png',
-      '/icon-512.png',
-      '/manifest.json'
-    ]);
-  }));
+self.addEventListener('install', (event) => {
+  console.log('Service Worker instalado.');
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
 });
